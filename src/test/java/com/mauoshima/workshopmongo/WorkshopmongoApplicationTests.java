a0f2f79d51462416f92300f82ea6b20e3b443d21
Mauricio Osima <kantai@uol.com.br>
2024-04-26 13:06:09 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/mauoshima/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauoshima.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
